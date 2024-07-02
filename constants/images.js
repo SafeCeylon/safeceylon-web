244043d42fa8bfe60e70e23852728af54ce04811
MCLakshan <mlakshancharitha@gmail.com>
2024-07-02 10:45:25 +0530

    resolved ui conflicts

--- constants/images.js
+++ constants/images.js
@@ -1,5 +1,5 @@
 import HomeBG from '@/public/assets/HomeBG.png';
-import DefaultBG from '@/public/assets/DefaultBG.png';
+// import DefaultBG from '@/public/assets/DefaultBG.png';
 import Logo1 from '@/public/assets/Logo1.png';
 import Logo2 from '@/public/assets/Logo2.png';
 import Logo3 from '@/public/assets/Logo3.png';
@@ -7,7 +7,7 @@ import Logo3 from '@/public/assets/Logo3.png';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   HomeBG,
-  DefaultBG,
+  // DefaultBG,
   Logo1,
   Logo2,
   Logo3,
