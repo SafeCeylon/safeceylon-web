d80d86b78ee26b89361b80546c9d977b6fe585f7
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2024-07-09 23:01:57 +0530

    ui changes

--- app/disaster/admin/page.tsx
+++ app/disaster/admin/page.tsx
@@ -1,5 +1,5 @@
-import DisasterNavbar from "@/components/DisasterNavbar";
-import RequestCard from "@/components/RequestCard";
+import DisasterNavbar from '@/components/DisasterNavbar';
+import RequestCard from '@/components/RequestCard';
 
 export default function Admin() {
   return (
@@ -8,12 +8,12 @@ export default function Admin() {
       <div className="px-[50px] md:px-[100px] flex h-4/5 w-full gap-20 mt-5">
         <div className="flex flex-col w-2/3 gap-14 h-full">
           <div className="flex w-full gap-20 h-1/4">
-            <div className="text-black flex flex-col bg-white w-1/2 items-center h-full rounded-2xl bg-[#FFF9F0]">
+            <div className="text-black flex flex-col w-1/2 items-center h-full rounded-2xl bg-[#FFF9F0]">
               <h4 className="text-lg mt-[10px]">Disaster Victims Status</h4>
               <div className="flex w-full gap-5 px-10 h-full my-5 ">
-                <RequestCard name="To Reply" percentage= {45} count={90} />
-                <RequestCard name="Replied" percentage={38} count={76}/>
-                <RequestCard name="Closed" percentage={17} count={34}/>
+                <RequestCard name="To Reply" percentage={45} count={90} />
+                <RequestCard name="Replied" percentage={38} count={76} />
+                <RequestCard name="Closed" percentage={17} count={34} />
               </div>
             </div>
             <div className="text-black flex flex-col bg-white w-1/2 items-center h-full rounded-2xl"></div>
--- components/RequestCard.tsx
+++ components/RequestCard.tsx
@@ -6,11 +6,17 @@ interface RequestCardProps {
   count: number;
 }
 
-const RequestCard: React.FC<RequestCardProps> = ({ name, percentage, count }) => {
+const RequestCard: React.FC<RequestCardProps> = ({
+  name,
+  percentage,
+  count,
+}) => {
   return (
-    <div className="flex flex-col items-center justify-center w-1/3 bg-white shadow shadow-lg shadow-[#00000030] bg-[#FFF9F0] rounded-xl">
+    <div className="flex flex-col items-center justify-center w-1/3 bg-white shadow-lg shadow-[#00000030]  rounded-xl">
       <h3 className="font-semibold text-[14px]">{name}</h3>
-      <h2 className="font-bold text-[32px] text-[#ff9900] ml-3">{percentage}%</h2>
+      <h2 className="font-bold text-[32px] text-[#ff9900] ml-3">
+        {percentage}%
+      </h2>
       <h4 className="font-light text-[12px]">({count})</h4>
     </div>
   );
