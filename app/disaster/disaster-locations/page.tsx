a893111f54e6c4c60ba6ce4100a260beae41e154
MCLakshan <mlakshancharitha@gmail.com>
2024-07-16 21:04:42 +0530

    added shadcn ui for dependencies

--- app/disaster/disaster-locations/page.tsx
+++ app/disaster/disaster-locations/page.tsx
@@ -7,7 +7,12 @@ export default function Admin() {
       <DisasterNavbar />
       <div className="px-[50px] md:px-[100px] flex h-4/5 w-full gap-20">
         <div className="flex flex-col w-full gap-14 h-full">
-          <div className="w-full bg-white h-full rounded-2xl"></div>
+          <div className="w-full bg-white h-full rounded-2xl">
+            <div className="w-full bg-orange-300 h-20 flex-shrink-0"></div>
+            <div className="w-full bg-slate-400 flex-grow">
+              hello
+            </div>
+          </div>
         </div>
       </div>
     </div>
