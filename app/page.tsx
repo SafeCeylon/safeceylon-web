b492a5c35a9e14521135a3875477637b01eb1d9e
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2024-07-23 10:09:58 +0530

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import { Poppins } from 'next/font/google';
 
+import Image from 'next/image';
+
+import images from '@/constants/images';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { LoginButton } from '@/components/auth/login-button';
@@ -8,17 +11,20 @@ const font = Poppins({ subsets: ['latin'], weight: ['600'] });
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <div className="space-y-6 text-center">
-        <h1
-          className={cn(
-            'text-6xl font-semibold text-white drop-shadow-md',
-            font.className,
-          )}
-        >
-          Auth
-        </h1>
-        <p>A simple authentication Service.</p>
+    <main className="bg-[url('../public/assets/HomeBG.png')] bg-cover bg-center  h-screen overflow-hidden flex justify-start lg:justify-end bg-[#0D0D0F] text-white">
+      <div className="flex flex-col lg:items-end lg:justify-center w-full lg:w-[50%] h-screen px-[50px] pt-[100px] lg:pt-0 md:px-[100px] lg:mt-[-170px]  lg:bg-transparent">
+        <Image
+          src={images.Logo1}
+          alt="Logo1"
+          className="w-auto lg:hidden-0 lg:h-[120px] xl:h-[150px] 2xl:h-[180px]"
+        />
+        <h3 className="text-3xl mt-5">
+          Revolutionizing Disaster Management and Safety.
+        </h3>
+        <p className="mt-5">
+          SafeCeylon is a robust and comprehensive system designed to enhance
+          disaster management and safety for all.
+        </p>
         <div>
           <LoginButton>
             <Button variant="secondary" size="lg">
