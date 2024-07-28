68d20fc2e75228a1eb32ecbb4fed648460bc622d
MCLakshan <mlakshancharitha@gmail.com>
2024-07-28 18:02:20 +0530

    finished disaster-officer-details page and created a blank page for officer-details-profile page

---
+++ app/(protected)/disaster/officer-details/profile/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from "@/components/ui/input"
+
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+import add_icon from '@/public/assets/add_icon.svg';
+import DefaultOfficerImage from '@/public/assets/default_officer_image.png';
+import Updateicon from '@/public/assets/update_icon.svg';
+import DeleteIcon from '@/public/assets/delete_icon.svg';
+
+export default function Admin() {
+  return (
+    <div className="px-[50px] md:px-[100px] flex h-4/5 w-full gap-20">
+      <div className="flex flex-col w-full gap-14 h-full">
+        <div className="w-full bg-white h-full rounded-2xl p-5 pt-2">
+          
+          
+
+
+          
+        </div>
+      </div>
+    </div>
+  );
+}
