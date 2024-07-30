74d7f38cc6083e31d235e82d5a2cb4c8fb589315
janindu-kulathilka <86515184+janindu-kulathilaka@users.noreply.github.com>
2024-07-30 19:05:10 +0530

    text align

--- app/(protected)/user/download/page.tsx
+++ app/(protected)/user/download/page.tsx
@@ -10,8 +10,7 @@ export default function Admin() {
   return (
     <div className="px-[50px] md:px-[100px] flex h-4/5 w-full gap-20 mt-5">
       <div className="flex justify-center">
-        <Image src={UserLogin} alt="mobile app" className="h-full" />
-        <div className="w-2/3 h-full flex flex-col gap-14 text-center items-center">
+      <div className="w-2/3 h-full flex flex-col gap-14 text-left mt-[60px]">
           <h1 className="text-6xl font-semibold text-white">
             Download
             <br />
@@ -43,6 +42,7 @@ export default function Admin() {
             </a>
           </div>
         </div>
+        <Image src={UserLogin} alt="mobile app" className="h-full" />
         <Image src={UserApp} alt="mobile app" className="h-full" />
       </div>
     </div>
